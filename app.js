050ed19114b1d20d7130a9d9292b82c5dcc12c36
kshitiz-swd <kshitiz123srivastava@gmail.com>
2025-07-08 15:01:35 +0530

    c

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://mediconnect-fpiijl8d1-kshitijs-projects-5b929cca.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://mediconnect-eight.vercel.app/'],
   credentials: true              
 }));
 app.use(bodyParser.json());
